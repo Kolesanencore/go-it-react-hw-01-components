8444159221e018c4913f28524f2f6d8f12cf2573
Kolesanencore <kolesanencore@gmail.com>
2023-05-02 22:45:36 +0300

    Completed HW

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ items }) => {
+  return (
+    <ul className={css.list}>
+      {items.map(item => (
+        <li className={css.item} key={item.id}>
+          <img src={item.avatar} alt={item.alt} />
+          <p className={css.name}>{item.name}</p>
+          <span className={item.isOnline ? css.true : css.false}></span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.string,
+    })
+  ),
+};
+
+export default FriendList;
